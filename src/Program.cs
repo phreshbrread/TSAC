8dbc1d184428dc394c8462cdc3aa7d835b4332ba
Brad Parker <crustydog9@gmail.com>
2020-08-18 17:59:31 +1000

    Add files via upload

--- src/Program.cs
+++ src/Program.cs
@@ -6,6 +6,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using TSAC;
 using TSAC.src;
+using TSAC.src.shapes;
 
 namespace TSAC
 {
@@ -24,11 +25,13 @@ namespace TSAC
             Cylinder cylinder = new Cylinder();
             Cube cube = new Cube();
             Rectangle rectangle = new Rectangle();
+            SBP sbp = new SBP();
 
             Console.WriteLine("Which TSA to calculate?");
             Console.WriteLine("1. Cube");
             Console.WriteLine("2. Rectangle");
             Console.WriteLine("3. Cylinder");
+            Console.WriteLine("4. Square Based Pyramid");
 
             var input = Console.ReadLine();
             if (input == "1")
@@ -42,6 +45,10 @@ namespace TSAC
             else if (input == "3")
             {
                 cylinder.Main();
+            }
+            else if (input == "4")
+            {
+                sbp.Main();
             }
             {
                 Console.WriteLine("Please select a valid option.");
